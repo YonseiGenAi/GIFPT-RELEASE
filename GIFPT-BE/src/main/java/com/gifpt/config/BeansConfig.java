c1860d0dbe165520955f7ddf65d26e11bdd54e72
우태호 <{>
2025-11-24 03:51:41 +0900

    [FIX] fix

--- GIFPT-BE/src/main/java/com/gifpt/config/BeansConfig.java
+++ GIFPT-BE/src/main/java/com/gifpt/config/BeansConfig.java
@@ -4,17 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.client.RestClient;
 
 @Configuration
 public class BeansConfig {
   @Bean
   public PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
-  }
-
-  @Bean
-  public RestClient restClient() {
-    return RestClient.create();
   }
 }
--- GIFPT-BE/src/main/java/com/gifpt/workspace/config/RestClientConfig.java
+++ GIFPT-BE/src/main/java/com/gifpt/workspace/config/RestClientConfig.java
@@ -8,11 +8,9 @@ import org.springframework.web.client.RestClient;
 public class RestClientConfig {
 
     @Bean
-    public RestClient workspaceRestClient() {
-        // 도커 컴포즈 안에서 django 서비스 이름으로 접속
-        // docker-compose.yml에서 django 서비스 이름이 'django' 이니까 이렇게 둠
-        return RestClient.builder()
-                .baseUrl("http://django:8000")   // 장고 서버 내부 주소
-                .build();
+    public RestClient restClient() {
+        // 기본 RestClient 빈 (baseUrl 없이 생성)
+        // 각 서비스에서 필요에 따라 baseUrl을 설정하거나 전체 URL을 사용할 수 있음
+        return RestClient.create();
     }
 }
